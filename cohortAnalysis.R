83c0b89a763a431a53ac0c80eb5eb6b5cf581b7e
Timothy Laurent <timothyjlaurent@gmail.com>
2013-08-06 11:35:38 -0700

    Initial plots exploring BMI, age, country of origin, and BMI status

---
+++ cohortAnalysis.R
@@ -0,0 +1,26 @@
+## this script will generate visuale to cohort for intelligent
+## selection of test subjects.
+install.packages("ggplot2")
+
+library( ggplot2 )
+
+CohortStats <- read.table("~/bioinf/GutMetagenomics/CohortStats.txt", header=T, quote="\"")
+cohort_Contig_Stats <- read.table("~/bioinf/GutMetagenomics/cohort_Contig_Stats.txt", header=T, quote="\"")
+cohort_read_stats <- read.table("~/bioinf/GutMetagenomics/cohort_read_stats.txt", header=T, quote="\"")
+CohortStats <- merge(CohortStats, cohort_Contig_Stats)
+CohortStats <- merge(CohortStats, cohort_read_stats)
+
+plot <- qplot(BMI, data = CohortStats, geom = "histogram", binwidth = 1, main= "BMI histogram", xlab="BMI\nbinwidth = 1")
+ggsave(filename = "BMI_hist_binwidth_1.pdf", plot = plot, width = 7 , height = 7)
+
+qplot(BMI, data = CohortStats, geom = "density", main= "BMI density plot", xlab="BMI" )
+
+
+qplot( Age, BMI, data = CohortStats , colour = Gender, main = "Age Vs BMI plot\ncolored by gender")
+
+ggsave(filename = "Age_vs_BMI_coloredBy_Sex.pdf", width = 7 , height = 7)
+
+qplot( Age, BMI, data = CohortStats , colour = Country ,shape= Gender, main = "Age Vs BMI plot\ncolored by country")
+
+ggsave(filename = "Age_vs_BMI_coloredBy_Country.pdf", width = 7 , height = 7)
+
